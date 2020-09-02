6d1fc3d81d9fadd318c987ed03e70b313c3498af
Abinash Panda <abinashp@protonmail.ch>
2020-09-02 20:20:57 +0530

    draw grid and acors fn added

--- index.js
+++ index.js
@@ -193,3 +193,35 @@ class DOMDisplay {
 		this.dom.remove()
 	}
 }
+
+function drawGrid(level) {
+	return elt(
+		"table",
+		{
+			class: "background",
+			style: `width: ${level.width * scale}px`,
+		},
+		...level.rows.map((row) =>
+			elt(
+				"tr",
+				{ style: `height: ${scale}px` },
+				...row.map((type) => elt("td", { class: type })),
+			),
+		),
+	)
+}
+
+function drawActors(actors) {
+	return elt(
+		"div",
+		{},
+		...actors.map((actor) => {
+			let rect = elt("div", { class: `actor ${actor.type}` })
+			rect.style.width = `${actor.size.x * scale}px`
+			rect.style.height = `${actor.size.y * scale}px`
+			rect.style.left = `${actor.pos.x * scale}px`
+			rect.style.top = `${actor.pos.y * scale}px`
+			return rect
+		}),
+	)
+}
